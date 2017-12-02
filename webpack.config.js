2c1cbde8fb8afa4bfbe6a97aa3fceca952f0f1ea
Mark Tofan <mark_tofan@yahoo.com>
2017-12-02 14:35:42 +0200

    Added the webpack config and the packages that will be used for the UI
    react, redux, redux-router
    Sass (scss) for style
    DEV: babel, eslint, redux-devtools, dev-server etc

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'app-js'),
+  entry: ['./js/main-app.js', './css/main.scss'],
+
+  output: {
+    filename: './bundle.js',
+    path: path.resolve(__dirname, 'app-js', 'dist'),
+    publicPath: '/dist',
+  },
+
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: ['babel-loader'],
+    },
+
+    {
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        use: ['css-loader', 'sass-loader'],
+        fallback: 'style-loader',
+      }),
+    },
+    ],
+  },
+
+  plugins: [
+    new ExtractTextPlugin({
+      filename: '[name].css',
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+  ],
+
+  devServer: {
+    clientLogLevel: 'none',
+    compress: true,
+    contentBase: path.resolve(__dirname, 'app-js'),
+    historyApiFallback: true,
+    hot: true,
+    port: 3001,
+    watchOptions: {
+      ignored: /node_modules/,
+    },
+  },
+};
